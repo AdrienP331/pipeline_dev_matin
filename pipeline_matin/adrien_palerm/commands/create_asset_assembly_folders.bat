ace3e5921aa0d798bd57c524147cd9e14809312a
Adrien Palerm <48124302+AdrienP331@users.noreply.github.com>
2024-09-22 23:12:14 +0200

    Add files via upload

---
+++ pipeline_matin/adrien_palerm/commands/create_asset_assembly_folders.bat
@@ -0,0 +1,3 @@
+@echo off
+"%~dp0..\python\python.exe" "%~dp0..\scripts\create_asset_assembly_folders.py" %*
+pause
---
+++ pipeline_matin/adrien_palerm/commands/create_light_render_folders.bat
@@ -0,0 +1,3 @@
+@echo off
+"%~dp0..\python\python.exe" "%~dp0..\scripts\create_light_render_folders.py" %*
+pause
